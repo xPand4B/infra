99dab9238a21a5e1fe3332cfa5e7c96aa15079af
Eric Heinzl <38140317+xPand4B@users.noreply.github.com>
2023-05-03 20:56:09 +0200

    Update setup-raspbian.sh

--- setup/raspi/setup-raspbian.sh
+++ setup/raspi/setup-raspbian.sh
@@ -93,6 +93,7 @@ sudo apt-get install libffi-dev libssl-dev
 sudo apt install python3-dev
 sudo apt-get install -y python3 python3-pip
 sudo pip3 install docker-compose
+sudo chmod 666 /var/run/docker.sock
 sudo systemctl enable docker
 
 updatePackages
